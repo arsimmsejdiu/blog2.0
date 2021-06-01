9d125b12d121c3a3630218d4590a339e0d69b642
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-01 16:08:38 +0200

    added the global styles variables

---
+++ src/components/navigation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <nav className="site-navigatopn">
+      <span>Blog2.0</span>
+      <ul>
+          <li>
+              
+          </li>
+      </ul>
+    </nav>
+  );
+}
