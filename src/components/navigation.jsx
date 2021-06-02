ddbe55153266fc1e331868024a3353fdfec17cef
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-02 22:02:00 +0200

    changes made in navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { navLink } from '../data/nav.data';
 
 export default function Navigation() {
   return (
     <nav className="site-navigatopn">
       <span>Blog2.0</span>
       <ul>
-          <li>
-              
-          </li>
+          {navLink.map((nav, i) => (
+              <li key={i}>
+                  <a href={nav.path}>{nav.title}</a>
+              </li>
+          ))}
       </ul>
     </nav>
   );
---
+++ src/data/nav.data.js
@@ -0,0 +1,26 @@
+export const navLink = [
+    {
+        title: 'Home',
+        path: '/'
+    },
+    {
+        title: 'Blogs',
+        path: '/blogs'
+    },
+    {
+        title: 'About',
+        path: '/about',
+    },
+    {
+        title: "Contact",
+        path: '/contact'
+    },
+    {
+        title: 'Write',
+        path: '/write',
+    },
+    {
+        title: 'Login',
+        path: '/login'
+    }
+]
