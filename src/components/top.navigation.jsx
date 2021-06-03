862b1fe46f8d2864397b7e164b2b2b1ceda4d637
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-03 16:52:25 +0200

    added a top navigation bar

---
+++ src/components/top.navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopNavigation = () => {
+    return (
+        <div className="top-nav">
+            <span></span>
+        </div>
+    )
+}
+
+export default TopNavigation;
