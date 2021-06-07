7a6abef2a968cdf575d4d40ea3efcacf5ece1bf3
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-07 12:00:46 +0200

    logo design

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,11 +3,15 @@ import { navLink } from '../data/nav.data';
 import { Link } from 'react-router-dom';
 import Lang from '../Images/translation.svg';
 import SearchBar from "./SearchBar";
+import Logo from '../Images/Logo.png';
 
 export default function Navigation() {
   return (
     <nav className="site-navigation">
-      <span>leoel.</span>
+      <span>
+        <img src={Logo} alt={Logo} />
+        Leutrim Olluri
+      </span>
       <SearchBar />
       <ul>
           {navLink.map((nav, i) => (
